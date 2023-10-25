2e8d5a98f47bed352bbe425f3cd95d264eac1c9e
Ghanem Lamloumi <ghanem.lamloumi1@gmail.com>
2023-10-26 00:47:59 +0200

    added cookieparser

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const jwt = require('jsonwebtoken');
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
 const User = require('./Models/User');
 
 dotenv.config();
@@ -11,6 +12,7 @@ mongoose.connect(process.env.DB);
 const jwtSecret = process.env.JWT_SECRET;
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 
 app.use(cors({
   credentials: true,
@@ -21,6 +23,21 @@ app.get('/test', (req, res) => {
   res.json('test ok')
 })
 
+app.get('/profile', (req, res) => {
+  const token = req.cookies?.token
+  if (token) {
+    jwt.verify(token, jwtSecret, {}, (err, userData) => {
+      if (err) {
+        throw err
+      }
+      res.json(userData)
+    })
+  } else {
+    res.status(401).json('no token')
+  }
+
+})
+
 app.post('/register', async (req, res) => {
   const { username, password } = req.body;
   try {
@@ -29,11 +46,11 @@ app.post('/register', async (req, res) => {
       if (err) {
         throw err
       }
-      res.cookie('token', token).json({ id: createdUser._id }).sendStatus(201)
+      res.cookie('token', token).json({ id: createdUser._id, username }).status(201)
     })
   } catch (error) {
     if (error) throw error
-    res.sendStatus(500).json('error')
+    res.json('error').status(500)
   }
 
 
