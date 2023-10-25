a6548356701e9512904ec07ee46a66421448a104
Ghanem Lamloumi <ghanem.lamloumi1@gmail.com>
2023-10-26 00:11:46 +0200

    fixed cors and jwt

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const jwt = require('jsonwebtoken');
+const cors = require('cors');
 const User = require('./Models/User');
 
 dotenv.config();
@@ -9,6 +10,12 @@ dotenv.config();
 mongoose.connect(process.env.DB);
 const jwtSecret = process.env.JWT_SECRET;
 const app = express();
+app.use(express.json());
+
+app.use(cors({
+  credentials: true,
+  origin: process.env.CLIENT_URL
+}));
 
 app.get('/test', (req, res) => {
   res.json('test ok')
@@ -16,13 +23,20 @@ app.get('/test', (req, res) => {
 
 app.post('/register', async (req, res) => {
   const { username, password } = req.body;
-  const createdUser = await User.create({ username, password })
-  jwt.sign({ userId: createdUser._id }, jwtSecret, (err, token) => {
-    if (err) {
-      throw err
-    }
-    res.cookie('token', token).sendStatus(201).json('ok')
-  })
+  try {
+    const createdUser = await User.create({ username, password })
+    jwt.sign({ userId: createdUser._id }, jwtSecret, {}, (err, token) => {
+      if (err) {
+        throw err
+      }
+      res.cookie('token', token).json({ id: createdUser._id }).sendStatus(201)
+    })
+  } catch (error) {
+    if (error) throw error
+    res.sendStatus(500).json('error')
+  }
+
+
 })
 app.listen(9000)
 
